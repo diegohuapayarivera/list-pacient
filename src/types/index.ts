c0223ce8aec2a945589308101303bd8381f95b99
diegohuapayarivera <diego.huapaya@vallegrande.edu.pe>
2024-09-07 15:55:03 -0500

    add store Patient and component PatientForm

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type Patient = {
+    id: string,
+    name: string,
+    caretaker: string,
+    email: string,
+    date: Date,
+    symtoms: string,
+}
+
+export type DraftPatient = Omit<Patient, "id">
